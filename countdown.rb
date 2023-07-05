fca472024c282d352fb672d6fa05f7b9f2eaa706
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-05 12:27:16 +0300

    first commit

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sleep(1) # Sleep for 1 second
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
